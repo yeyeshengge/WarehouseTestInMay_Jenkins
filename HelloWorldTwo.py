76cfe541e5419478076a21a123cbd1fd2a8e762d
二哥 <1123918878@qq.com>
2020-06-05 16:55:56 +0800

    注释测试身份证号

--- HelloWorldTwo.py
+++ HelloWorldTwo.py
@@ -1,11 +1,11 @@
 import requests
 
-string = input('请输入您的身份证号：')
-url = 'https://api.binstd.com/idcard/query'
-data = {'appkey':'3b92869ba2c908c2',
-        'idcard':string}
-response = requests.post(url,data)
-print(response.text)
+# string = input('请输入您的身份证号：')
+# url = 'https://api.binstd.com/idcard/query'
+# data = {'appkey':'3b92869ba2c908c2',
+#         'idcard':string}
+# response = requests.post(url,data)
+# print(response.text)
 
 print('HelloWorldTwo1')
 print('HelloWorldTwo2')
